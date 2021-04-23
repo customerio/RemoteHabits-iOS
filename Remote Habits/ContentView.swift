3e58b2d17e4911596731e508522eaf56167d21b1
Levi Bostian <levi@customer.io>
2021-04-23 11:44:14 -0500

    chore: initial commit. create new app in xcode

---
+++ Remote Habits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Remote Habits
+//
+//  Created by Levi Bostian on 4/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Remote Habits/Remote_HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Remote_HabitsApp.swift
+//  Remote Habits
+//
+//  Created by Levi Bostian on 4/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Remote_HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
