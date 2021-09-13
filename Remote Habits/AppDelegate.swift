3cd28ecbb8b684f018b420b5c1edb9f753519c8d
Levi Bostian <levi@customer.io>
2021-09-13 14:01:38 -0500

    docs: explain initialize SDK singleton [skip ci]

--- Remote Habits/AppDelegate.swift
+++ Remote Habits/AppDelegate.swift
@@ -10,7 +10,9 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     ) -> Bool {
         FirebaseApp.configure() // run first so crashes are caught before all other initialization.
 
-        CustomerIO.initialize(siteId: "", apiKey: "", region: Region.US)
+        // If you have decided to use the shared singleton method of using the Customer.io SDK,
+        // initialize the SDK here in the `AppDelegate`.
+        CustomerIO.initialize(siteId: "YOUR SITE ID", apiKey: "YOUR API KEY", region: Region.US)
 
         return true
     }
