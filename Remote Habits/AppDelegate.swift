34dac4394dec5d3a6dfd9810ef1944ccf85692b3
Levi Bostian <levi@customer.io>
2021-09-20 09:42:58 -0500

    feat: register APN device token to profile (#15)

--- Remote Habits/AppDelegate.swift
+++ Remote Habits/AppDelegate.swift
@@ -1,3 +1,4 @@
+import CioMessagingPushAPN
 import CioTracking
 import Firebase
 import Foundation
@@ -14,6 +15,54 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         // initialize the SDK here in the `AppDelegate`.
         CustomerIO.initialize(siteId: "YOUR SITE ID", apiKey: "YOUR API KEY", region: Region.US)
 
+        // It's good practice to always register for remote push when the app starts.
+        // This asserts that the Customer.io SDK always has a valid APN device token to use.
+        UIApplication.shared.registerForRemoteNotifications()
+
         return true
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let cioMessagingPush = DI.shared.messagingPush
+        let logger = DI.shared.logger
+        let cioErrorUtil = DI.shared.customerIOErrorUtil
+        var userManager = DI.shared.userManager
+
+        // At this time, the Customer.io SDK does not save APN device tokens to a customer profile for you.
+        // So, we are saving the device token for later when a profile is identified in the app to register
+        // a device token to the profile then.
+        userManager.apnDeviceToken = deviceToken
+
+        // Customer.io SDK will return an error when attempting to register a device token if a profile has
+        // not been identified with Customer.io yet. Therefore, we check if a profile has been identified yet.
+        guard userManager.isLoggedIn else {
+            return
+        }
+
+        cioMessagingPush
+            .application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken) { result in
+                switch result {
+                case .success:
+                    logger.log("Successfully registered device push token")
+                case .failure(let cioError):
+                    cioErrorUtil.parse(cioError)
+                }
+            }
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        let logger = DI.shared.logger
+        let cioErrorUtil = DI.shared.customerIOErrorUtil
+        let cioMessagingPush = DI.shared.messagingPush
+
+        // This will delete the existing push token from the identified profile in Customer.io SDK.
+        cioMessagingPush.application(application, didFailToRegisterForRemoteNotificationsWithError: error) { result in
+            switch result {
+            case .success:
+                logger.log("Successfully reported error to CIO SDK for registered push notifications")
+            case .failure(let cioError):
+                cioErrorUtil.parse(cioError)
+            }
+        }
     }
 }
--- Remote Habits/autogenerated/DIDependencies.swift
+++ Remote Habits/autogenerated/DIDependencies.swift
@@ -1,5 +1,7 @@
+import CioMessagingPush
 import CioTracking
 import Foundation
+import UIKit
 
 // sourcery: InjectRegister = "Tracking"
 // sourcery: InjectCustom
@@ -20,5 +22,25 @@ extension DI {
         let region = Env.customerIORegion == "us" ? Region.US : Region.EU
 
         return CustomerIO(siteId: Env.customerIOSiteId, apiKey: Env.customerIOApiKey, region: region)
+    }
+}
+
+// sourcery: InjectRegister = "MessagingPush"
+// sourcery: InjectCustom
+extension MessagingPush {}
+
+extension DI {
+    var customMessagingPush: MessagingPush {
+        MessagingPush(customerIO: DI.shared.customerIO)
+    }
+}
+
+// sourcery: InjectRegister = "UserDefaults"
+// sourcery: InjectCustom
+extension UserDefaults {}
+
+extension DI {
+    var customUserDefaults: UserDefaults {
+        UserDefaults.standard
     }
 }
