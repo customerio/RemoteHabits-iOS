6e5bedee3cab2babd839e04fb61fc5063e255db1
ami-aman <91549653+ami-aman@users.noreply.github.com>
2021-12-13 19:14:52 +0530

    fix for PR comment - added colors as an asset

--- Remote Habits/Common/RHFont+Color.swift
+++ Remote Habits/Common/RHFont+Color.swift
@@ -15,13 +15,13 @@ struct RHFont {
 
 // TODO - Move to colour assets
 struct RHColor {
-    static let LabelGray = UIColor(red: 0.235, green: 0.235, blue: 0.263, alpha: 0.6)
-    static let LineGray = UIColor(red: 0.776, green: 0.776, blue: 0.784, alpha: 1)
-    static let SelectedLineGray = UIColor(red: 0.650, green: 0.650, blue: 0.650, alpha: 1)
-    static let DefaultBackground = UIColor(red: 247/255, green: 248/255, blue: 250/255, alpha: 1)
-    static let ButtonEnabled = UIColor(red: 10/225, green: 132/225, blue: 255/225, alpha: 1.0)
-    static let ButtonDisabled = UIColor(red: 205/225, green: 205/225, blue: 205/225, alpha: 1.0)
-    static let TextDisabled = UIColor(red: 138/225, green: 141/225, blue: 144/225, alpha: 1.0)
+    static let LabelGray = UIColor(named: "LabelGray") ?? UIColor.gray
+    static let LineGray = UIColor(named: "LineGray") ?? UIColor.gray
+    static let SelectedLineGray = UIColor(named: "SelectedLineGray") ?? UIColor.gray
+    static let DefaultBackground = UIColor(named: "DefaultBackground")
+    static let ButtonEnabled = UIColor(named: "ButtonEnabled")
+    static let ButtonDisabled = UIColor(named: "ButtonDisabled")
+    static let TextDisabled = UIColor(named : "TextDisabled") ?? UIColor.gray
 }
 
 
