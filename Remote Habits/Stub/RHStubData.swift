0d565b1bf5b90da131e651ed1c5bbf9ef74e03dc
ami-aman <91549653+ami-aman@users.noreply.github.com>
2022-01-06 20:23:07 +0530

    lint code

--- Remote Habits/Stub/RHStubData.swift
+++ Remote Habits/Stub/RHStubData.swift
@@ -9,10 +9,10 @@ class RemoteHabitsData {
     func getHabitHeaders() -> [HabitHeadersInfo] {
         
         let title = isLoggedIn ? userManager.userName : "Guest"
-        let section_first = HabitHeadersInfo(headerTitle: "\(title ?? "Your")'s Habits", titleFontSize: 34, titleFontName: "SFProDisplay-Bold", ids: [1,2,3])
-        let section_second = HabitHeadersInfo(headerTitle: "Details", titleFontSize: 17, titleFontName: "SFProDisplay-Bold", ids: [4,5,6])
+        let sectionfirst = HabitHeadersInfo(headerTitle: "\(title ?? "Your")'s Habits", titleFontSize: 34, titleFontName: "SFProDisplay-Bold", ids: [1,2,3])
+        let sectionsecond = HabitHeadersInfo(headerTitle: "Details", titleFontSize: 17, titleFontName: "SFProDisplay-Bold", ids: [4,5,6])
        
-        return [section_first, section_second]
+        return [sectionfirst, sectionsecond]
     }
     
     func getHabitsData() -> [HabitsData] {
@@ -32,7 +32,7 @@ class RemoteHabitsData {
                                      frequency: 8,
                                      startTime: nil,
                                      endTime: nil,
-                                     habitDescription: "Drinking enough water each day is crucial for many reasons: to regulate body temperature, keep joints lubricated, prevent infections, deliver nutrients to cells, and keep organs functioning properly. Being well-hydrated also improves sleep quality, cognition, and mood.",
+                                   habitDescription: AppStrings.habitInfoHydrationDescription.localized,
                                      actionName: nil,
                                      actionType: nil)
         
@@ -45,7 +45,7 @@ class RemoteHabitsData {
                                         frequency: 5,
                                         startTime: nil,
                                         endTime: nil,
-                                        habitDescription: "Taking a break improves focus and concentration and provides the opportunity for an employee's mental reset. After a break, work can resume with more energy and motivation. Working without taking one or more breaks only leads to mental and physical fatigue. It can even lead to burnout in the long run.",
+                                        habitDescription: AppStrings.habitInfoBreakDescription.localized,
                                         actionName: nil,
                                         actionType: nil)
         
@@ -58,7 +58,7 @@ class RemoteHabitsData {
                                         frequency: 2,
                                         startTime: nil,
                                         endTime: nil,
-                                        habitDescription: "Having scheduled, uninterrupted time to focus on deep work provides employees an opportunity to complete difficult tasks, produce high quality output, and generate new ideas. Time to focus increases productivity, improves decision making, and boosts creativity.",
+                                        habitDescription: AppStrings.habitInfoFocusDescription.localized,
                                         actionName: nil,
                                         actionType: nil)
         
@@ -84,7 +84,7 @@ class RemoteHabitsData {
         let sdkData     = HabitsData(id: 6,
                                          icon: "phone",
                                          title: "SDK",
-                                         subtitle: "Customer.io iOS Client/1.0.0-alpha.18 (iPhone13,2; iOS 15.1) Remote Habits/pr.28",
+                                         subtitle: RHConstants.mockSdkInfo,
                                          type: ElementType.none,
                                          isEnabled: nil,
                                          frequency: nil,
