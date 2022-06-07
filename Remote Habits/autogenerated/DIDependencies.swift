d0efaa4939e4097c75f8f15330f8804aeae68016
Levi Bostian <levi@customer.io>
2022-06-07 11:51:52 -0500

    fix: get app to compile from removing SDK classes (#68)

--- Remote Habits/autogenerated/DIDependencies.swift
+++ Remote Habits/autogenerated/DIDependencies.swift
@@ -3,31 +3,13 @@ import CioTracking
 import Foundation
 import UIKit
 
-// sourcery: InjectRegister = "Tracking"
-// sourcery: InjectCustom
-extension Tracking {}
-
-extension DI {
-    var customTracking: Tracking {
-        Tracking(customerIO: customerIO)
-    }
-}
-
 // sourcery: InjectRegister = "CustomerIO"
 // sourcery: InjectCustom
 extension CustomerIO {}
 
 extension DI {
     var customCustomerIO: CustomerIO {
-        let region = Env.customerIORegion == "us" ? Region.US : Region.EU
-
-        let cio = CustomerIO(siteId: Env.customerIOSiteId, apiKey: Env.customerIOApiKey, region: region)
-
-        cio.config {
-            $0.logLevel = .debug
-        }
-
-        return cio
+        CustomerIO.shared
     }
 }
 
@@ -37,7 +19,7 @@ extension MessagingPush {}
 
 extension DI {
     var customMessagingPush: MessagingPush {
-        MessagingPush(customerIO: DI.shared.customerIO)
+        MessagingPush.shared
     }
 }
 
