f58d727d3a7157d4c7cd0abcea8bc4ca7f7107a4
ami-aman <91549653+ami-aman@users.noreply.github.com>
2022-07-28 00:44:12 +0400

    cleaned Login Router

--- Remote Habits/Router/LoginRouter.swift
+++ Remote Habits/Router/LoginRouter.swift
@@ -4,7 +4,6 @@ import UIKit
 protocol LoginRouting {
     func routeToDashboard()
     func routeToWorkspace(withData: WorkspaceData?)
-    func routeToConfigureCioSdk()
 }
 
 class LoginRouter: LoginRouting {
@@ -19,12 +18,6 @@ class LoginRouter: LoginRouting {
     func routeToWorkspace(withData workspaceData: WorkspaceData?) {
         let viewController = SwitchWorkspaceViewController.newInstance()
         viewController.workspaceData = workspaceData
-        let navigation = UINavigationController(rootViewController: viewController)
-        loginViewController?.present(navigation, animated: true, completion: nil)
-    }
-
-    func routeToConfigureCioSdk() {
-        let viewController = ConfigureCioSdkViewController.newInstance()
         let navigation = UINavigationController(rootViewController: viewController)
         loginViewController?.present(navigation, animated: true, completion: nil)
     }
