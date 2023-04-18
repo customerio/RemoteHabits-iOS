ca24c545d9037672fcd64c02ab4251a8a8403936
Shahroz Khan <shrozali@gmail.com>
2023-04-18 15:04:07 +0500

    lint and generate

--- Remote Habits/autogenerated/AutoMockable.generated.swift
+++ Remote Habits/autogenerated/AutoMockable.generated.swift
@@ -1,4 +1,4 @@
-// Generated using Sourcery 2.0.1 — https://github.com/krzysztofzablocki/Sourcery
+// Generated using Sourcery 2.0.2 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 // swiftlint:disable all
 
@@ -6,64 +6,78 @@ import Foundation
 #if canImport(FoundationNetworking)
 import FoundationNetworking
 #endif
-import CioMessagingPushAPN
 import CioTracking
+import CioMessagingPushAPN
 
 /**
- ######################################################
- Documentation
- ######################################################
+######################################################
+Documentation
+######################################################
 
- This automatically generated file you are viewing contains mock classes that you can use in your test suite.
+This automatically generated file you are viewing contains mock classes that you can use in your test suite. 
 
- * How do you generate a new mock class?
+* How do you generate a new mock class? 
 
- 1. Mocks are generated from Swift protocols. So, you must make one.
+1. Mocks are generated from Swift protocols. So, you must make one. 
 
- ```
- protocol FriendsRepository {
-     func acceptFriendRequest(_ onComplete: @escaping () -> Void)
- }
+```
+protocol FriendsRepository {
+    func acceptFriendRequest(_ onComplete: @escaping () -> Void)
+}
 
- class AppFriendsRepository: FriendsRepository {
-     ...
- }
- ```
+class AppFriendsRepository: FriendsRepository {
+    ...
+}
+```
 
- 2. Have your new protocol extend `AutoMockable`:
+2. Have your new protocol extend `AutoMockable`:
 
- ```
- protocol FriendsRepository: AutoMockable {
- ```
+```
+protocol FriendsRepository: AutoMockable {
+```
 
- 3. Run the command `make generate` on your machine. The new mock should be added to this file.
+3. Run the command `make generate` on your machine. The new mock should be added to this file. 
 
- * How do you use the mocks in your test class?
+* How do you use the mocks in your test class? 
 
- ```
- class ExampleViewModelTest: XCTestCase {
-     var viewModel: ExampleViewModel!
-     var exampleRepositoryMock: ExampleRepositoryMock!
-     override func setUp() {
-         exampleRepositoryMock = ExampleRepositoryMock()
-         viewModel = AppExampleViewModel(exampleRepository: exampleRepositoryMock)
-     }
- }
- ```
+```
+class ExampleViewModelTest: XCTestCase {
+    var viewModel: ExampleViewModel!
+    var exampleRepositoryMock: ExampleRepositoryMock!
+    override func setUp() {
+        exampleRepositoryMock = ExampleRepositoryMock()
+        viewModel = AppExampleViewModel(exampleRepository: exampleRepositoryMock)
+    }
+}
+```
 
- Or, you may need to inject the mock in a different way using the DI.shared graph:
+Or, you may need to inject the mock in a different way using the DI.shared graph:
 
- ```
- class ExampleTest: XCTestCase {
-     var exampleViewModelMock: ExampleViewModelMock!
-     var example: Example!
-     override func setUp() {
-         exampleViewModelMock = ExampleViewModelMock()
-         DI.shared.override(.exampleViewModel, value: exampleViewModelMock, forType: ExampleViewModel.self)
-         example = Example()
-     }
- }
+```
+class ExampleTest: XCTestCase {
+    var exampleViewModelMock: ExampleViewModelMock!
+    var example: Example!
+    override func setUp() {
+        exampleViewModelMock = ExampleViewModelMock()
+        DI.shared.override(.exampleViewModel, value: exampleViewModelMock, forType: ExampleViewModel.self)
+        example = Example()
+    }
+}
 
- ```
+```
 
- */
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
