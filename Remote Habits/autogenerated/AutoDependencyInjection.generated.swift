be4889a88d67f583db54da8662c8f637779df784
Shahroz Khan <shrozali@gmail.com>
2023-04-18 15:04:43 +0500

    fix

--- Remote Habits/autogenerated/AutoDependencyInjection.generated.swift
+++ Remote Habits/autogenerated/AutoDependencyInjection.generated.swift
@@ -2,64 +2,64 @@
 // DO NOT EDIT
 // swiftlint:disable all
 
-import Foundation
-import CioTracking
 import CioMessagingPushAPN
+import CioTracking
+import Foundation
 
 // File generated from Sourcery-DI project: https://github.com/levibostian/Sourcery-DI
 // Template version 1.0.0
 
 /**
-######################################################
-Documentation
-######################################################
+ ######################################################
+ Documentation
+ ######################################################
 
-This automatically generated file you are viewing is a dependency injection graph for your app's source code.
-You may be wondering a couple of questions. 
+ This automatically generated file you are viewing is a dependency injection graph for your app's source code.
+ You may be wondering a couple of questions.
 
-1. How did this file get generated? Answer --> https://github.com/levibostian/Sourcery-DI#how
-2. Why use this dependency injection graph instead of X other solution/tool? Answer --> https://github.com/levibostian/Sourcery-DI#why-use-this-project
-3. How do I add dependencies to this graph file? Follow one of the instructions below:
-* Add a non singleton class: https://github.com/levibostian/Sourcery-DI#add-a-non-singleton-class
-* Add a generic class: https://github.com/levibostian/Sourcery-DI#add-a-generic-class
-* Add a singleton class: https://github.com/levibostian/Sourcery-DI#add-a-singleton-class
-* Add a class from a 3rd party library/SDK: https://github.com/levibostian/Sourcery-DI#add-a-class-from-a-3rd-party
-* Add a `typealias` https://github.com/levibostian/Sourcery-DI#add-a-typealias
+ 1. How did this file get generated? Answer --> https://github.com/levibostian/Sourcery-DI#how
+ 2. Why use this dependency injection graph instead of X other solution/tool? Answer --> https://github.com/levibostian/Sourcery-DI#why-use-this-project
+ 3. How do I add dependencies to this graph file? Follow one of the instructions below:
+ * Add a non singleton class: https://github.com/levibostian/Sourcery-DI#add-a-non-singleton-class
+ * Add a generic class: https://github.com/levibostian/Sourcery-DI#add-a-generic-class
+ * Add a singleton class: https://github.com/levibostian/Sourcery-DI#add-a-singleton-class
+ * Add a class from a 3rd party library/SDK: https://github.com/levibostian/Sourcery-DI#add-a-class-from-a-3rd-party
+ * Add a `typealias` https://github.com/levibostian/Sourcery-DI#add-a-typealias
 
-4. How do I get dependencies from the graph in my code? 
-```
-// If you have a class like this:
-class OffRoadWheels {}
+ 4. How do I get dependencies from the graph in my code?
+ ```
+ // If you have a class like this:
+ class OffRoadWheels {}
 
-class ViewController: UIViewController {
-    // Call the property getter to get your dependency from the graph: 
-    let wheels = DI.shared.offRoadWheels
-    // note the name of the property is name of the class with the first letter lowercase. 
+ class ViewController: UIViewController {
+     // Call the property getter to get your dependency from the graph:
+     let wheels = DI.shared.offRoadWheels
+     // note the name of the property is name of the class with the first letter lowercase.
 
-    // you can also use this syntax instead:
-    let wheels: OffRoadWheels = DI.shared.inject(.offRoadWheels)
-    // although, it's not recommended because `inject()` performs a force-cast which could cause a runtime crash of your app. 
-}
-```
+     // you can also use this syntax instead:
+     let wheels: OffRoadWheels = DI.shared.inject(.offRoadWheels)
+     // although, it's not recommended because `inject()` performs a force-cast which could cause a runtime crash of your app.
+ }
+ ```
 
-5. How do I use this graph in my test suite? 
-```
-let mockOffRoadWheels = // make a mock of OffRoadWheels class 
-DI.shared.override(.offRoadWheels, mockOffRoadWheels) 
-```
+ 5. How do I use this graph in my test suite?
+ ```
+ let mockOffRoadWheels = // make a mock of OffRoadWheels class
+ DI.shared.override(.offRoadWheels, mockOffRoadWheels)
+ ```
 
-Then, when your test function finishes, reset the graph:
-```
-DI.shared.resetOverrides()
-```
+ Then, when your test function finishes, reset the graph:
+ ```
+ DI.shared.resetOverrides()
+ ```
 
-*/
-
-/** 
- enum that contains list of all dependencies in our app. 
- This allows automated unit testing against our dependency graph + ability to override nodes in graph. 
  */
- enum Dependency: CaseIterable {
+
+/**
+ enum that contains list of all dependencies in our app.
+ This allows automated unit testing against our dependency graph + ability to override nodes in graph.
+ */
+enum Dependency: CaseIterable {
     case customerIOErrorUtil
     case deepLinksHandlerUtil
     case logger
@@ -74,172 +74,191 @@ DI.shared.resetOverrides()
     case userDefaults
 }
 
-
 /**
- Dependency injection graph specifically with dependencies in the  module. 
+ Dependency injection graph specifically with dependencies in the  module.
 
- We must use 1+ different graphs because of the hierarchy of modules in this SDK. 
- Example: You can't add classes from `Tracking` module in `Common`'s DI graph. However, classes 
- in `Common` module can be in the `Tracking` module. 
+ We must use 1+ different graphs because of the hierarchy of modules in this SDK.
+ Example: You can't add classes from `Tracking` module in `Common`'s DI graph. However, classes
+ in `Common` module can be in the `Tracking` module.
  */
- class DI {
-     static var shared: DI = DI()
+class DI {
+    static var shared: DI = .init()
     private var overrides: [Dependency: Any] = [:]
-    private init() {
+    private init() {}
+
+    /**
+     Designed to be used only in test classes to override dependencies.
+
+     ```
+     let mockOffRoadWheels = // make a mock of OffRoadWheels class
+     DI.shared.override(.offRoadWheels, mockOffRoadWheels)
+     ```
+     */
+    func override<Value: Any>(_ dep: Dependency, value: Value, forType type: Value.Type) {
+        overrides[dep] = value
     }
 
     /**
-    Designed to be used only in test classes to override dependencies. 
-
-    ```
-    let mockOffRoadWheels = // make a mock of OffRoadWheels class 
-    DI.shared.override(.offRoadWheels, mockOffRoadWheels) 
-    ```
-    */
-     func override<Value: Any>(_ dep: Dependency, value: Value, forType type: Value.Type) {
-        overrides[dep] = value 
-    }
-
-    /**
-    Reset overrides. Meant to be used in `tearDown()` of tests. 
-    */
-     func resetOverrides() {        
+     Reset overrides. Meant to be used in `tearDown()` of tests.
+     */
+    func resetOverrides() {
         overrides = [:]
     }
 
     /**
-    Use this generic method of getting a dependency, if you wish. 
-    */
-     func inject<T>(_ dep: Dependency) -> T {                            
+     Use this generic method of getting a dependency, if you wish.
+     */
+    func inject<T>(_ dep: Dependency) -> T {
         switch dep {
-            case .customerIOErrorUtil: return self.customerIOErrorUtil as! T 
-            case .deepLinksHandlerUtil: return self.deepLinksHandlerUtil as! T 
-            case .logger: return self.logger as! T 
-            case .notificationUtil: return self.notificationUtil as! T 
-            case .profileRepository: return self.profileRepository as! T 
-            case .userManager: return self.userManager as! T 
-            case .customerIO: return self.customerIO as! T 
-            case .habitDataManager: return self.habitDataManager as! T 
-            case .messagingPush: return self.messagingPush as! T 
-            case .profileViewModel: return self.profileViewModel as! T 
-            case .trackerViewModel: return self.trackerViewModel as! T 
-            case .userDefaults: return self.userDefaults as! T 
+        case .customerIOErrorUtil: return customerIOErrorUtil as! T
+        case .deepLinksHandlerUtil: return deepLinksHandlerUtil as! T
+        case .logger: return logger as! T
+        case .notificationUtil: return notificationUtil as! T
+        case .profileRepository: return profileRepository as! T
+        case .userManager: return userManager as! T
+        case .customerIO: return customerIO as! T
+        case .habitDataManager: return habitDataManager as! T
+        case .messagingPush: return messagingPush as! T
+        case .profileViewModel: return profileViewModel as! T
+        case .trackerViewModel: return trackerViewModel as! T
+        case .userDefaults: return userDefaults as! T
         }
     }
 
     /**
-    Use the property accessors below to inject pre-typed dependencies. 
-    */
+     Use the property accessors below to inject pre-typed dependencies.
+     */
 
     // CustomerIOErrorUtil
-    internal var customerIOErrorUtil: CustomerIOErrorUtil {    
-        if let overridenDep = self.overrides[.customerIOErrorUtil] {
+    internal var customerIOErrorUtil: CustomerIOErrorUtil {
+        if let overridenDep = overrides[.customerIOErrorUtil] {
             return overridenDep as! CustomerIOErrorUtil
         }
-        return self.newCustomerIOErrorUtil
+        return newCustomerIOErrorUtil
     }
-    private var newCustomerIOErrorUtil: CustomerIOErrorUtil {    
-        return AppCustomerIOErrorUtil(logger: self.logger)
+
+    private var newCustomerIOErrorUtil: CustomerIOErrorUtil {
+        AppCustomerIOErrorUtil(logger: logger)
     }
+
     // DeepLinksHandlerUtil
-    internal var deepLinksHandlerUtil: DeepLinksHandlerUtil {    
-        if let overridenDep = self.overrides[.deepLinksHandlerUtil] {
+    internal var deepLinksHandlerUtil: DeepLinksHandlerUtil {
+        if let overridenDep = overrides[.deepLinksHandlerUtil] {
             return overridenDep as! DeepLinksHandlerUtil
         }
-        return self.newDeepLinksHandlerUtil
+        return newDeepLinksHandlerUtil
     }
-    private var newDeepLinksHandlerUtil: DeepLinksHandlerUtil {    
-        return AppDeepLinksHandlerUtil(userManager: self.userManager)
+
+    private var newDeepLinksHandlerUtil: DeepLinksHandlerUtil {
+        AppDeepLinksHandlerUtil(userManager: userManager)
     }
+
     // Logger
-    internal var logger: Logger {    
-        if let overridenDep = self.overrides[.logger] {
+    internal var logger: Logger {
+        if let overridenDep = overrides[.logger] {
             return overridenDep as! Logger
         }
-        return self.newLogger
+        return newLogger
     }
-    private var newLogger: Logger {    
-        return AppLogger()
+
+    private var newLogger: Logger {
+        AppLogger()
     }
+
     // NotificationUtil
-    internal var notificationUtil: NotificationUtil {    
-        if let overridenDep = self.overrides[.notificationUtil] {
+    internal var notificationUtil: NotificationUtil {
+        if let overridenDep = overrides[.notificationUtil] {
             return overridenDep as! NotificationUtil
         }
-        return self.newNotificationUtil
+        return newNotificationUtil
     }
-    private var newNotificationUtil: NotificationUtil {    
-        return AppNotificationUtil()
+
+    private var newNotificationUtil: NotificationUtil {
+        AppNotificationUtil()
     }
+
     // ProfileRepository
-    internal var profileRepository: ProfileRepository {    
-        if let overridenDep = self.overrides[.profileRepository] {
+    internal var profileRepository: ProfileRepository {
+        if let overridenDep = overrides[.profileRepository] {
             return overridenDep as! ProfileRepository
         }
-        return self.newProfileRepository
+        return newProfileRepository
     }
-    private var newProfileRepository: ProfileRepository {    
-        return AppProfileRepository(cio: self.customerIO, cioErrorUtil: self.customerIOErrorUtil, userManager: self.userManager, messagingPush: self.messagingPush)
+
+    private var newProfileRepository: ProfileRepository {
+        AppProfileRepository(cio: customerIO, cioErrorUtil: customerIOErrorUtil, userManager: userManager,
+                             messagingPush: messagingPush)
     }
+
     // UserManager
-    internal var userManager: UserManager {    
-        if let overridenDep = self.overrides[.userManager] {
+    internal var userManager: UserManager {
+        if let overridenDep = overrides[.userManager] {
             return overridenDep as! UserManager
         }
-        return self.newUserManager
+        return newUserManager
     }
-    private var newUserManager: UserManager {    
-        return AppUserManager(userDefaults: self.userDefaults)
+
+    private var newUserManager: UserManager {
+        AppUserManager(userDefaults: userDefaults)
     }
+
     // CustomerIO (custom. property getter provided via extension)
-    internal var customerIO: CustomerIO {    
-        if let overridenDep = self.overrides[.customerIO] {
+    internal var customerIO: CustomerIO {
+        if let overridenDep = overrides[.customerIO] {
             return overridenDep as! CustomerIO
         }
-        return self.customCustomerIO
+        return customCustomerIO
     }
+
     // HabitDataManager
-    internal var habitDataManager: HabitDataManager {    
-        if let overridenDep = self.overrides[.habitDataManager] {
+    internal var habitDataManager: HabitDataManager {
+        if let overridenDep = overrides[.habitDataManager] {
             return overridenDep as! HabitDataManager
         }
-        return self.newHabitDataManager
+        return newHabitDataManager
     }
-    private var newHabitDataManager: HabitDataManager {    
-        return HabitDataManager()
+
+    private var newHabitDataManager: HabitDataManager {
+        HabitDataManager()
     }
+
     // MessagingPush (custom. property getter provided via extension)
-    internal var messagingPush: MessagingPush {    
-        if let overridenDep = self.overrides[.messagingPush] {
+    internal var messagingPush: MessagingPush {
+        if let overridenDep = overrides[.messagingPush] {
             return overridenDep as! MessagingPush
         }
-        return self.customMessagingPush
+        return customMessagingPush
     }
+
     // ProfileViewModel
-    internal var profileViewModel: ProfileViewModel {    
-        if let overridenDep = self.overrides[.profileViewModel] {
+    internal var profileViewModel: ProfileViewModel {
+        if let overridenDep = overrides[.profileViewModel] {
             return overridenDep as! ProfileViewModel
         }
-        return self.newProfileViewModel
+        return newProfileViewModel
     }
-    private var newProfileViewModel: ProfileViewModel {    
-        return ProfileViewModel(cio: self.customerIO, profileRepository: self.profileRepository, notificationUtil: self.notificationUtil)
+
+    private var newProfileViewModel: ProfileViewModel {
+        ProfileViewModel(cio: customerIO, profileRepository: profileRepository, notificationUtil: notificationUtil)
     }
+
     // TrackerViewModel
-    internal var trackerViewModel: TrackerViewModel {    
-        if let overridenDep = self.overrides[.trackerViewModel] {
+    internal var trackerViewModel: TrackerViewModel {
+        if let overridenDep = overrides[.trackerViewModel] {
             return overridenDep as! TrackerViewModel
         }
-        return self.newTrackerViewModel
+        return newTrackerViewModel
     }
-    private var newTrackerViewModel: TrackerViewModel {    
-        return TrackerViewModel(cio: self.customerIO)
+
+    private var newTrackerViewModel: TrackerViewModel {
+        TrackerViewModel(cio: customerIO)
     }
+
     // UserDefaults (custom. property getter provided via extension)
-    internal var userDefaults: UserDefaults {    
-        if let overridenDep = self.overrides[.userDefaults] {
+    internal var userDefaults: UserDefaults {
+        if let overridenDep = overrides[.userDefaults] {
             return overridenDep as! UserDefaults
         }
-        return self.customUserDefaults
+        return customUserDefaults
     }
 }
